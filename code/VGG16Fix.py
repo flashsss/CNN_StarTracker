97bbdbc3afcd89b5023f99e7ec1523ba7f68c58c
flashsss <flasheinstein92@gmail.com>
2022-01-28 16:46:51 +0700

    fixx bgt

--- code/VGG16Fix.py
+++ code/VGG16Fix.py
@@ -38,6 +38,7 @@ AUTOTUNE = tf.data.AUTOTUNE
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
+normalization_layer = layers.Rescaling(1./255)
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
 first_image = image_batch[0]
