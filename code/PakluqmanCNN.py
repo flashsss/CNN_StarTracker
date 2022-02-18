eb10e3a7eae327fc325741b8c079716667412e9f
flashsss <flasheinstein92@gmail.com>
2022-02-18 08:07:36 +0700

    50

--- code/PakluqmanCNN.py
+++ code/PakluqmanCNN.py
@@ -54,7 +54,7 @@ cnn.add(tf.keras.layers.Dense(480,activation='softmax'))
 #Compiling the CNN
 cnn.compile(loss='categorical_crossentropy',optimizer='rmsprop',metrics=['accuracy'])
 #Training the CNN on the Training set and evaluating it on the Test set
-cnn.fit(x=training_set,validation_data=test_set,epochs=15)
+cnn.fit(x=training_set,validation_data=test_set,epochs=50)
 
 #SAVING THE MODEL
 cnn.save('./Results/preprocessed_features_model.h5')
