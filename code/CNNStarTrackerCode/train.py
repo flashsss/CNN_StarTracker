525f8aaa8c1e12c207ab39dfd4927c6312615a54
flashsss <flasheinstein92@gmail.com>
2022-02-20 13:07:43 +0700

    train

--- code/CNNStarTrackerCode/train.py
+++ code/CNNStarTrackerCode/train.py
@@ -1,37 +1,86 @@
-""## Import the necessary libraries"""
+"""## Import the necessary libraries"""
 
-import os
-import random as rd
-import shutil
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
 
-"""## Load Local Modules"""
+"""# CNN Main"""
 
-def get_files_and_shuffle(path):
-    files = list(os.listdir(path))
-    shuffled = rd.sample(files,len(files))
-    return shuffled
-
-def extract_from_list(list,ratio):
-    new_list = []
-    number_of_files = int(len(list)*ratio)
-    for i in range(number_of_files):
-        new_list.append(list[i])
-    return new_list
-
-"""# Train Test Split - Main"""
-
+#To prevent overfitting -> training on the same images
+#DATA PREPROCESSING
+#TRAIN
 featureMethod = 'multitriangle'
 path = './dataset/'+featureMethod
-folders = os.listdir(path+'/train/') #['0', '1', '2', '3', '4', '5', '6', '7']
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+training_set = train_datagen.flow_from_directory(
+    path+'/train',
+    target_size=(616,820),
+    batch_size=32,
+    class_mode='categorical'
+)
 
-for folder in folders:
-    shuffled = get_files_and_shuffle(path+'/train/'+str(folder)) #List of shuffled list -> name of files of type string
-    test_files = extract_from_list(shuffled,0.15)
-    for filename in test_files:
-        shutil.move(path+'/train/'+str(folder)+'/'+str(filename),path+'/test/'+str(folder)+'/'+str(filename))
+#TEST
+test_datagen = ImageDataGenerator(rescale=1./255)
+test_set = test_datagen.flow_from_directory(
+    path+'/test',
+    target_size=(616,820),
+    batch_size=32,
+    class_mode='categorical'
+)
 
-number_of_train = len(os.listdir(path+'/train/0/'))
-number_of_test = len(os.listdir(path+'/test/0/'))
-print("Number of training files per class: ",number_of_train)
-print("Number of testing files per class: ",number_of_test)
-print("Ratio of test/train: ",(number_of_test/number_of_train))
+#BUILDING THE CONVOLUTIONAL NEURAL NETWORK
+cnn = tf.keras.models.Sequential() #Sequence of layers
+#CONVOLUTION 1
+cnn.add(tf.keras.layers.Conv2D(filters=128,kernel_size=3,activation='relu',input_shape=[616,820,3]))
+#POOLING 1
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2,strides=2))
+#CONVOLUTION 2
+cnn.add(tf.keras.layers.Conv2D(filters=64,kernel_size=3,activation='relu'))
+#POOLING 2
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2,strides=2))
+#CONVOLUTION 3
+cnn.add(tf.keras.layers.Conv2D(filters=32,kernel_size=3,activation='relu'))
+#POOLING 3
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2,strides=2))
+
+#FLATTENING
+cnn.add(tf.keras.layers.Flatten())
+#FULL CONNECTION
+cnn.add(tf.keras.layers.Dense(units=256,activation='relu'))
+cnn.add(tf.keras.layers.Dense(units=128,activation='relu'))
+cnn.add(tf.keras.layers.Dense(480,activation='softmax'))
+
+#TRAINING THE CONVOLUTIONAL NEURAL NETWORK
+#Compiling the CNN
+cnn.compile(loss='categorical_crossentropy',optimizer='rmsprop',metrics=['accuracy'])
+#Training the CNN on the Training set and evaluating it on the Test set
+history = cnn.fit(x=training_set,validation_data=test_set,epochs=15)
+
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('./Results/accuracy.pdf')
+
+plt.clf()
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('./Results/loss.pdf')
+
+
+#SAVING THE MODEL
+cnn.save('./Results/preprocessed_features_model.h5')
