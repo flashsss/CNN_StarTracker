a4e8f548de5c4464bba258aac9778e56594803a6
flashsss <flasheinstein92@gmail.com>
2022-03-08 14:44:14 +0700

    hoaho

--- code/CNNStarTrackerCode/AlexNet.py
+++ code/CNNStarTrackerCode/AlexNet.py
@@ -31,15 +31,15 @@ cnn.add(tf.keras.layers.Conv2D(filters=96,kernel_size=(11,11),strides=4,activati
 #POOLING 1
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=(3,3),strides=2))
 #CONVOLUTION 2
-cnn.add(tf.keras.layers.Conv2D(filters=256,kernel_size=(5,5),strides=1,padding='2',activation='relu'))
+cnn.add(tf.keras.layers.Conv2D(filters=256,kernel_size=(5,5),strides=1,activation='relu'))
 #POOLING 2
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=(3,3),strides=2))
 #CONVOLUTION 3
-cnn.add(tf.keras.layers.Conv2D(filters=384,kernel_size=(3,3),strides=1,padding='same',activation='relu'))
+cnn.add(tf.keras.layers.Conv2D(filters=384,kernel_size=(3,3),strides=1,activation='relu'))
 #CONVOLUTION 4
-cnn.add(tf.keras.layers.Conv2D(filters=384,kernel_size=(3,3),strides=1,padding='same',activation='relu'))
+cnn.add(tf.keras.layers.Conv2D(filters=384,kernel_size=(3,3),strides=1,activation='relu'))
 #CONVOLUTION 5
-cnn.add(tf.keras.layers.Conv2D(filters=256,kernel_size=(3,3),strides=1,padding='same',activation='relu'))
+cnn.add(tf.keras.layers.Conv2D(filters=256,kernel_size=(3,3),strides=1,activation='relu'))
 #POOLING 3
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=(3,3),strides=2))
 
