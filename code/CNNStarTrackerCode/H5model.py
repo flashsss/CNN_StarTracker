3405370703a3b2a04d5fdbbc5b541f71dc22b8c5
flashsss <flasheinstein92@gmail.com>
2022-03-10 14:12:54 +0700

    sasf

--- code/CNNStarTrackerCode/H5model.py
+++ code/CNNStarTrackerCode/H5model.py
@@ -1,6 +1,13 @@
 from tensorflow.keras.models import load_model
 import cv2
+import matplotlib.pyplot as plt
 model = load_model('./Results/preprocessed_features_model.h5')
+
+def displayImg(img,cmap='gray'):
+    fig = plt.figure(figsize=(12,10))
+    ax = fig.add_subplot(111)
+    ax.imshow(img,cmap)
+    plt.show()
 
 img = cv2.imread("./class_image_4/300.jpg") 
 # img = multitriangles_detector(img,5)
